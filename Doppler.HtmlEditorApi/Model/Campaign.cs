4ecb7036da0960d15114220f734945f18b02e356
Cromeror <cristobalromerorossi@gmail.com>
2022-02-02 15:11:49 -0500

    feat: required attributes to campaign content request

--- Doppler.HtmlEditorApi/Model/Campaign.cs
+++ Doppler.HtmlEditorApi/Model/Campaign.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace Doppler.HtmlEditorApi.Model;
 
-public record CampaignContent(JsonElement meta, string htmlContent) : Content(meta, htmlContent);
+public record CampaignContent(
+    [Required]
+    JsonElement meta,
+    [Required]
+    string htmlContent) : Content(meta, htmlContent);
