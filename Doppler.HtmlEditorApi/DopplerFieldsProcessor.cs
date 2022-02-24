b95a8e62578bded3b4bfaf383b15d69a3cebca04
Andrés Moschini <andresmoschini@gmail.com>
2022-02-24 15:35:33 -0300

    feat: remove all field-ids

--- Doppler.HtmlEditorApi/DopplerFieldsProcessor.cs
+++ Doppler.HtmlEditorApi/DopplerFieldsProcessor.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Doppler.HtmlEditorApi;
 
 /// <summary>
@@ -6,6 +8,10 @@ namespace Doppler.HtmlEditorApi;
 /// </summary>
 public class DopplerFieldsProcessor
 {
+    public const string FIELD_START_DELIMITER_BACK_END = "|*|";
+    public const string FIELD_END_DELIMITER_BACK_END = "*|*";
+    public static readonly Regex BACKEND_FIELD_REGEX = new Regex($@"{Regex.Escape(FIELD_START_DELIMITER_BACK_END)}(\d+){Regex.Escape(FIELD_END_DELIMITER_BACK_END)}");
+
     public DopplerFieldsProcessor()
     {
         // TODO: receive and prepare fields information
@@ -16,6 +22,11 @@ public class DopplerFieldsProcessor
         => inputHtml;
 
     public string ClearInexistentFieldIs(string inputHtml)
-        // TODO: complete the behavior here
-        => inputHtml;
+        => BACKEND_FIELD_REGEX.Replace(
+            inputHtml,
+            match => FieldIdExist(int.Parse(match.Groups[1].Value)) ? match.Value : string.Empty);
+
+    private bool FieldIdExist(int fieldId)
+        // TODO: use field information to decide it
+        => false;
 }
