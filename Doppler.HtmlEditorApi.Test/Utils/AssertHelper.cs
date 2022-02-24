b4eb5d88fdbcdf62b6bc7ef07e484d9f5de6f24e
Andrés Moschini <andresmoschini@gmail.com>
2022-02-24 12:54:43 -0300

    feat: support storing head in the same way than Doppler does

--- Doppler.HtmlEditorApi.Test/Utils/AssertHelper.cs
+++ Doppler.HtmlEditorApi.Test/Utils/AssertHelper.cs
@@ -1,9 +1,14 @@
+using System.Text.RegularExpressions;
 using ReflectionMagic;
+using Xunit;
 
 namespace Doppler.HtmlEditorApi.Test.Utils;
 
 public static class AssertHelper
 {
+    private static readonly Regex LINE_ENDINGS_REGEX = new Regex(@"[\r\n]");
+    private static readonly Regex SPACES_REGEX = new Regex(@"\s");
+
     public static bool GetValueAndContinue<T>(T input, out T output)
     {
         output = input;
@@ -15,4 +20,13 @@ public static class AssertHelper
         output = input.AsDynamic();
         return true;
     }
+
+    public static void EqualIgnoringSpaces(string expected, string actual)
+        => Assert.Equal(RemoveSpaces(expected), RemoveSpaces(actual));
+
+    public static string RemoveSpaces(string str)
+        => str == null ? null
+        : SPACES_REGEX.Replace(
+            LINE_ENDINGS_REGEX.Replace(str, string.Empty),
+            string.Empty);
 }
--- Doppler.HtmlEditorApi/DopplerHtmlParser.cs
+++ Doppler.HtmlEditorApi/DopplerHtmlParser.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+
 namespace Doppler.HtmlEditorApi;
 
 /// <summary>
@@ -10,15 +12,26 @@ public class DopplerHtmlParser
     // Old Doppler code:
     // https://github.com/MakingSense/Doppler/blob/ed24e901c990b7fb2eaeaed557c62c1adfa80215/Doppler.HypermediaAPI/ApiMappers/ToDoppler/CampaignContent_To_DtoContent.cs#L27-L29
     private readonly string _inputHtml;
+    private readonly HtmlDocument _htmlDocument;
+    private readonly HtmlNode _headNode;
 
     public DopplerHtmlParser(string inputHtml)
     {
         _inputHtml = inputHtml;
+        _htmlDocument = new HtmlDocument();
+        _htmlDocument.LoadHtml(inputHtml);
+        _headNode = _htmlDocument.DocumentNode.SelectSingleNode("//head");
     }
 
     public string GetDopplerContent()
-        => _inputHtml;
+        => EnsureContent(
+            _headNode == null ? _inputHtml
+                : _htmlDocument.DocumentNode.SelectSingleNode("//body")?.InnerHtml
+                ?? _inputHtml.Replace(_headNode.OuterHtml, ""));
 
     public string GetHeadContent()
-        => null;
+        => _headNode?.InnerHtml;
+
+    private static string EnsureContent(string htmlContent)
+        => string.IsNullOrWhiteSpace(htmlContent) ? "<BR>" : htmlContent;
 }
