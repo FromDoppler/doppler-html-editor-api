d0e61b261215311e3c05fd5d2e9c37d5b7918c4d
Andrés Moschini <andresmoschini@gmail.com>
2022-02-25 15:01:46 -0300

    feat: replace field names by field ids

--- Doppler.HtmlEditorApi/DopplerFieldsProcessor.cs
+++ Doppler.HtmlEditorApi/DopplerFieldsProcessor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -11,15 +12,23 @@ namespace Doppler.HtmlEditorApi;
 /// </summary>
 public class DopplerFieldsProcessor
 {
+    public const string FIELD_START_DELIMITER = "[[[";
+    public const string FIELD_END_DELIMITER = "]]]";
     public const string FIELD_START_DELIMITER_BACK_END = "|*|";
     public const string FIELD_END_DELIMITER_BACK_END = "*|*";
     public static readonly Regex BACKEND_FIELD_REGEX = new Regex($@"{Regex.Escape(FIELD_START_DELIMITER_BACK_END)}(\d+){Regex.Escape(FIELD_END_DELIMITER_BACK_END)}");
 
+    // % is here to accept %20
+    public static readonly Regex FIELD_REGEX = new Regex($@"{Regex.Escape(FIELD_START_DELIMITER)}([a-zA-Z0-9 \-_ñÑáéíóúÁÉÍÓÚ%]+){Regex.Escape(FIELD_END_DELIMITER)}");
+
+    private readonly ReadOnlyDictionary<string, int> FieldIdsByNameOrAlias;
     private readonly ReadOnlyDictionary<int, string> FieldNamesById;
 
     public DopplerFieldsProcessor(IEnumerable<Field> fields)
     {
-        // TODO: receive and prepare fields information
+        // TODO: support field aliases and %20 in place of spaces
+        FieldIdsByNameOrAlias = new ReadOnlyDictionary<string, int>(
+            fields.ToDictionary(x => x.name, x => x.id, StringComparer.OrdinalIgnoreCase));
 
         // Only canonical names
         FieldNamesById = new ReadOnlyDictionary<int, string>(
@@ -27,8 +36,11 @@ public class DopplerFieldsProcessor
     }
 
     public string ReplaceFieldNamesToFieldIdsInHtmlContent(string inputHtml)
-        // TODO: complete the behavior here
-        => inputHtml;
+        => FIELD_REGEX.Replace(
+            inputHtml,
+            match => FieldIdsByNameOrAlias.TryGetValue(match.Groups[1].Value, out var fieldId)
+                ? $"{FIELD_START_DELIMITER_BACK_END}{fieldId}{FIELD_END_DELIMITER_BACK_END}"
+                : match.Value);
 
     public string ClearInexistentFieldIs(string inputHtml)
         => BACKEND_FIELD_REGEX.Replace(
